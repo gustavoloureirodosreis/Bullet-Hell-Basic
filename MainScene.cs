68ee7c41578da482175b46a0802d24b7b1fb457b
Gustavo <gusts92@gmail.com>
2020-07-11 20:10:07 -0300

    @mainscene part 1/3

---
+++ MainScene.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public class MainScene : Node2D {
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready() {
+        
+    }
+
+    public override void _Process(float delta) {
+        
+    }
+
+    private void _on_HUD_StartGame() {
+        
+    }
+}
